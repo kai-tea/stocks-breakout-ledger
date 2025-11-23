e5116f9566db850306af2d1faaeba490cb8b7f23
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2025-11-23 16:17:39 +0100

    feat(storage): add parquet write/read
    - add 'src/ledger/storage.py'
    - implement 'add_entry', 'read_entries'

---
+++ src/ledger/storage.py
@@ -0,0 +1,30 @@
+import pandas as pd
+#from __future__ import annotations
+from pathlib import Path
+from datetime import datetime, date
+import uuid
+
+WAREHOUSE = Path("data/warehouse")
+WAREHOUSE.mkdir(parents=True, exist_ok=True)
+ENTRIES = WAREHOUSE / "entries.parquet"
+
+def read_entries(path: Path) -> pd.DataFrame:
+    if path.exists():
+        return pd.read_parquet(path)
+    else:
+        return pd.DataFrame()
+
+def add_entry(ticker: str, data: date):
+    df = read_entries(ENTRIES)
+    entry_id = str(uuid.uuid4())
+
+    row = {
+        "entry_id": entry_id,
+        "ticker": ticker,
+        "date": date,
+        "created_ad": datetime.today().strftime('%Y-%m-%d')
+    }
+
+    df = pd.concat([df, pd.DataFrame([row])])
+    df.to_parquet(ENTRIES)
+    return entry_id
