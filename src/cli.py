aae0a15c91ce8a14809cb3bc606e8e49853daccb
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2026-02-26 16:43:09 +0100

    feat(cli): add ticker and name as cli args to cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,1 +1,16 @@
 import typer
+
+from typing import Annotated
+
+app = typer.Typer()
+
+@app.command()
+
+def add(
+        ticker: str,
+        date: str
+):
+    print(f"Hello {ticker} world")
+
+if __name__ == "__main__":
+    app()
