f5024ab923d9fb55598e789604942abddff8affc
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2026-02-28 13:08:50 +0100

    fix(config): fix STOOQ_DIR typo

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 PROJECT_ROOT =  Path(__file__).resolve().parent.parent
 DATA_DIR =      PROJECT_ROOT / "data"
-STOOD_DIR =     PROJECT_ROOT / "stooq"
+STOOQ_DIR = PROJECT_ROOT / "stooq"
