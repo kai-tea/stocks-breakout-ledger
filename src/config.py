1611ce40c70b3fe677d9b2b9c09146117ec94d06
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2026-02-28 17:29:51 +0100

    chore(config): adds WAREHOUSE_DIR path shortcut

--- src/config.py
+++ src/config.py
@@ -2,4 +2,8 @@ from pathlib import Path
 
 PROJECT_ROOT =  Path(__file__).resolve().parent.parent
 DATA_DIR =      PROJECT_ROOT / "data"
-STOOQ_DIR = DATA_DIR / "stooq"
+STOOQ_DIR =     DATA_DIR / "stooq" # contains stooq stocks data
+WAREHOUSE_DIR = DATA_DIR / "warehouse" # contains parquet files that have been converted from .txt stooq files
+
+# ensure warehouse directory exists
+WAREHOUSE_DIR.mkdir(parents=True, exist_ok=True)
