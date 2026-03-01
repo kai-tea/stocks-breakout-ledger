edd14cc59355ff68a702dd14bb16742a34d1072e
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2026-03-01 13:53:14 +0100

    feat(src): change pandas print() formatting

--- src/cli.py
+++ src/cli.py
@@ -1,14 +1,18 @@
 import typer
-from typing import Annotated
+import pandas as pd
 from datetime import datetime
 
 import main
 from main import add
 
+# --- PANDAS DISPLAY CONFIG ---
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_columns", None)
+# -----------------------------
+
 app = typer.Typer()
 
 @app.command()
-
 def add(
         ticker: str,
         date: datetime = typer.Argument(..., formats=["%Y-%m-%d"]) # converts to datetime.strptime()
