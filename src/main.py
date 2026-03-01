16962c0e99feda808fbc9c1241a5cab07681214c
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2026-03-01 17:53:27 +0100

    style(main): cleanup

--- src/main.py
+++ src/main.py
@@ -9,15 +9,6 @@ from compute import compute
 from config import PROCESSED_DIR
 
 
-def append_result_to_csv(csv_file_path: Path, df: pd.DataFrame) -> None:
-    """creates csv file if not present and appends df"""
-
-    # creates header in csv if file does not exist yet
-    # and appends df to csv
-    csv_exists = os.path.isfile(csv_file_path)
-    df.to_csv(csv_file_path, mode='a', header=not csv_exists, index=False)
-
-
 def add(ticker: str, target_date: datetime) -> None:
     # fetch raw data
     try:
@@ -26,11 +17,20 @@ def add(ticker: str, target_date: datetime) -> None:
         print(f"Error: {e}")
 
     # compute indicators
-    df_result = compute(df, ticker, target_date)
+    df = compute(df, ticker, target_date)
 
+    # save results to csv
     csv_file_name = "test.csv"
     csv_file_path = PROCESSED_DIR/csv_file_name
-    append_result_to_csv(csv_file_path, df_result)
+    append_result_to_csv(csv_file_path, df)
 
     print(f"saved to:\tprocessed/{csv_file_name}")
+
+def append_result_to_csv(csv_file_path: Path, df: pd.DataFrame) -> None:
+    """creates csv file if not present and appends df"""
+
+    # creates header in csv if file does not exist yet
+    # and appends df to csv
+    csv_exists = os.path.isfile(csv_file_path)
+    df.to_csv(csv_file_path, mode='a', header=not csv_exists, index=False)
 
