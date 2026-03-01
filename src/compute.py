16a897e1a24b03fa3fa24624bfcfda9d5f6f718d
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2026-03-01 17:40:44 +0100

    feat(*): logic for appending results
    - appends results to csv in "warehouse/processed/"

--- src/compute.py
+++ src/compute.py
@@ -3,20 +3,38 @@ import pandas_ta as ta
 
 from datetime import datetime
 
+
 def compute_all(df: pd.DataFrame) -> pd.DataFrame:
     """calculates all indicators for all dates"""
 
     compute_df = df.copy()
 
-    compute_df.ta.sma(length=10, append=True)
-    compute_df.ta.sma(length=20, append=True)
-    compute_df.ta.sma(length=50, append=True)
+    compute_df = compute_sma(compute_df)
 
     return compute_df
 
-def compute(df: pd.DataFrame, target_date: datetime) -> pd.DataFrame:
+def compute(df: pd.DataFrame, ticker: str, target_date: datetime) -> pd.DataFrame:
     """returns calculated indicators for target_date"""
 
+    # compute all indicators
     df = compute_all(df)
 
-    return df.loc[str(target_date.date())]
+    # get results for target_date only
+    df = df.loc[[str(target_date)]]
+
+    # add a "bo_name" column as identifier for Breakouts (eg. "AAPL_2001_Jan")
+    year = target_date.year
+    month = target_date.strftime("%b")
+    # insert as first column
+    df.insert(0, "bo_name", f"{ticker.upper()}_{year}_{month}")
+
+    return df
+
+def compute_sma(df: pd.DataFrame) -> pd.DataFrame:
+    """computes 10 20 50 SMA and returns new df"""
+
+    df.ta.sma(length=10, append=True)
+    df.ta.sma(length=20, append=True)
+    df.ta.sma(length=50, append=True)
+
+    return df
--- src/main.py
+++ src/main.py
@@ -1,24 +1,36 @@
 from datetime import datetime
 from pathlib import Path
+import pandas as pd
+import os
 
-from util import get_path_from_filename
 from fetch import fetch
 from compute import compute
 
-from config import STOOQ_DIR
+from config import PROCESSED_DIR
+
+
+def append_result_to_csv(csv_file_path: Path, df: pd.DataFrame) -> None:
+    """creates csv file if not present and appends df"""
+
+    # creates header in csv if file does not exist yet
+    # and appends df to csv
+    csv_exists = os.path.isfile(csv_file_path)
+    df.to_csv(csv_file_path, mode='a', header=not csv_exists, index=False)
 
 
 def add(ticker: str, target_date: datetime) -> None:
+    # fetch raw data
     try:
         df = fetch(ticker)
     except FileNotFoundError as e:
         print(f"Error: {e}")
 
-    print(df.head(10))
+    # compute indicators
+    df_result = compute(df, ticker, target_date)
 
-    new = compute(df, target_date)
+    csv_file_name = "test.csv"
+    csv_file_path = PROCESSED_DIR/csv_file_name
+    append_result_to_csv(csv_file_path, df_result)
 
-    print(new)
-
-    return None
+    print(f"saved to:\tprocessed/{csv_file_name}")
 
