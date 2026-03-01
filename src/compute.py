135e756b01a601ab2ba6ebd7ec69954f324a10ad
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2026-03-01 10:48:13 +0100

    feat(compute): add basic logic

---
+++ src/compute.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import pandas_ta as ta
+
+from datetime import datetime
+
+def compute_all(df: pd.DataFrame) -> pd.DataFrame:
+    """calculates all indicators"""
+
+    compute_df = df.copy()
+
+    compute_df.ta.sma(length=10, append=True)
+    compute_df.ta.sma(length=20, append=True)
+    compute_df.ta.sma(length=50, append=True)
+
+    return compute_df
+
+def compute(df: pd.DataFrame, date: datetime) -> pd.DataFrame:
+    df = compute_all(df)
+
+    return df
