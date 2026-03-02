f72eade75ae14f6c9fda6ccc34039f188fce37cc
Kaite Zhou <YOUR_ID+USERNAME@users.noreply.github.com>
2026-03-02 14:16:04 +0100

    style(config): move pandas display config to top

--- src/config.py
+++ src/config.py
@@ -1,17 +1,19 @@
-from pathlib import Path
-
-PROJECT_ROOT =  Path(__file__).resolve().parent.parent
-DATA_DIR =      PROJECT_ROOT / "data"
-STOOQ_DIR =     DATA_DIR / "stooq" # contains stooq stocks data
-WAREHOUSE_DIR = DATA_DIR / "warehouse"
-CLEAN_DIR =     WAREHOUSE_DIR / "clean" # contains cleaned parquet files that have been converted from .txt stooq files
-PROCESSED_DIR = WAREHOUSE_DIR / "processed"
-
-# ensures directory exists
-CLEAN_DIR.mkdir(parents=True, exist_ok=True)
-PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
-
 # PANDAS DISPLAY CONFIG
 import pandas as pd
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", 1000)
+
+# default Paths
+from pathlib import Path
+
+PROJECT_ROOT =  Path(__file__).resolve().parent.parent
+DATA_DIR =      PROJECT_ROOT / "data"
+STOOQ_DIR =     DATA_DIR / "stooq" # contains raw stooq data
+WAREHOUSE_DIR = DATA_DIR / "warehouse"
+CLEAN_DIR =     WAREHOUSE_DIR / "clean" # contains cleaned parquet files of stocks
+PROCESSED_DIR = WAREHOUSE_DIR / "processed"
+
+# ensures directories exist
+CLEAN_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+
